d8b9bc50dcf856a1d59810b1a4b45ce3a87a6197
Vishal Kumar <8.vishal@gmail.com>
2020-04-08 04:46:53 +0530

    comit 2 master

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -22,9 +22,15 @@ namespace Vidly
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            /* Commented by Vishal
+             * bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                      */
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/site.css"));
+
         }
     }
 }
